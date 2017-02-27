8be3e2cd886b81cd30ec7d1eeb594eb61e4481db
Amy Allen <alle0283@algonquinlive.com>
2017-02-27 11:14:40 -0500

    Uploaded assignment files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var newItem = document.querySelector("#new-item");
+var list = document.querySelector("#list");
+var listItem;
+
+function removeItem() {
+	this.remove();
+} 
+
+function addItem() {
+	listItem = document.createElement("li");
+	listItemText = prompt("Enter a new item for to-do list:");
+	if (listItemText) {
+		listItem.innerHTML = listItemText;
+		list.appendChild(listItem);
+		listItem.addEventListener("click", removeItem, false);
+	}
+}
+
+newItem.addEventListener("click", addItem, false);
